b5c739f6a71af58a44c36fc5172ebbbad59a5de1
Rodrigo Ordóñez <rodrigo.op1791@gmail.com>
2018-11-11 12:05:33 -0600

    Cambios menores

--- footer.php
+++ footer.php
@@ -1,6 +1,113 @@
 
+
+		
+			<div id="espacio"></div>
+			<div id="mosaico">		
+				<section id="imgPrincipal">
+					<img src="ejemplo2.jpg" width="100%" height="100%">	
+				</section>
+				<section id="imgSecundarias">
+					<img src="ejemplo2.jpg" width="49%" height="49%">	
+					<img src="ejemplo2.jpg" width="49%" height="49%">	
+					<img src="ejemplo2.jpg" width="49%" height="49%">	
+					<img src="ejemplo2.jpg" width="49%" height="49%">	
+				</section>
+			</div>
+			<hr id="barra">
+			<div class="contenedorNoticias">
+				<div class="noticias">
+					<div class="tituloSeccion">TITULO SECCION</div>
+					<div class="noticia">
+						<div class="imgNoticia">
+							<img  src="ejemplo2.jpg" width="100%">
+						</div>
+						<div class="txtNoticia">
+							<div class="tituloNoticia">Titulo Noticia</div>
+							<div class="resumenNoticia">
+								Lorem ipsum dolor sit amet
+							</div>
+						</div>
+					</div>
+					<div class="noticia">
+						<div class="imgNoticia">
+							<img  src="ejemplo2.jpg" width="100%">
+						</div>
+						<div class="txtNoticia">
+							<div class="tituloNoticia">Titulo Noticia</div>
+							<div class="resumenNoticia">
+								Lorem ipsum dolor sit amet
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="noticias">
+					<div class="tituloSeccion">TITULO SECCION</div>
+					<div class="noticia">
+						<div class="imgNoticia">
+							<img  src="ejemplo2.jpg" width="100%">
+						</div>
+						<div class="txtNoticia">
+							<div class="tituloNoticia">Titulo Noticia</div>
+							<div class="resumenNoticia">
+								Lorem ipsum dolor sit amet
+							</div>
+						</div>
+					</div>
+					<div class="noticia">
+						<div class="imgNoticia">
+							<img  src="ejemplo2.jpg" width="100%">
+						</div>
+						<div class="txtNoticia">
+							<div class="tituloNoticia">Titulo Noticia</div>
+							<div class="resumenNoticia">
+								Lorem ipsum dolor sit amet
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<hr id="barra">
+			<div class="contenedorNoticias">
+				<div class="noticiaCuadro">
+					<div class="imgNoticiaCuadro">
+							<img  src="ejemplo.png" width="100%">
+					</div>
+					<div class="texto">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.
+					</div>
+				</div>
+				<div class="noticiaCuadro">
+					<div class="imgNoticiaCuadro">
+							<img  src="ejemplo.png" width="100%">
+					</div>
+					<div class="texto">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.
+					</div>
+				</div>
+				<div class="noticiaCuadro">
+					<div class="imgNoticiaCuadro">
+							<img  src="ejemplo.png" width="100%">
+					</div>
+					<div class="texto">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.
+					</div>
+				</div>
+				<div class="noticiaCuadro">
+					<div class="imgNoticiaCuadro">
+							<img  src="ejemplo.png" width="100%">
+					</div>
+					<div class="texto">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.
+					</div>
+				</div>
+			</div>
+		</div>
 		<footer>
-			<h1> This is my footer</h1>
+			
 		</footer>
 
 	<?php wp_footer(); ?>
