9a7928b9bcfb8a0715618f1a500ac4a22188a98c
Rodrigo Ordóñez <rodrigo.op1791@gmail.com>
2018-11-11 12:05:18 -0600

    Inclusión de Bootstrap
    CSS Y JS

--- functions.php
+++ functions.php
@@ -1,11 +1,28 @@
 <?php
-	
+	/*  ===============
+		Incluir scripts
+		===============	*/
 	function encuentroinf_script_enqueue() {
+		//CSS
+		wp_enqueue_style('bootstrap', get_template_directory_uri().'/css/bootstrap.min.css', array(), '4.1.3', 'all');
 		wp_enqueue_style('eiestilo', get_template_directory_uri().'/css/encuentro.css', array(), '1.0.0', 'all');
+
+		//JS
+		wp_enqueue_script('jquery');
+		wp_enqueue_script('bootstrapjs', get_template_directory_uri().'/js/bootstrap.min.js', array(), '1.0.0', true);
 		wp_enqueue_script('eijs', get_template_directory_uri().'/js/encuentro.js', array(), '1.0.0', true);
 
+		
+		
 	}
 
+	// Registrar navbar
+    require_once('class-wp-bootstrap-navwalker.php');
+	
+
+	/*  ===============
+		Activar menús
+		===============	*/
 	function encuentroinf_setup() {
 		
 		add_theme_support('menus');
@@ -20,5 +37,5 @@
 
 	add_theme_support('post-thumbnails');
 
-	add_theme_support('post-formats');
+	add_theme_support('post-formats', array('aside', 'image', 'video'));
 	?>
